208fe908f66eba94f50f19eb56ebf10e663d826f
Sireesha-Budideti <sireeshabudideti@gmail.com>
2025-06-25 16:02:06 +0530

    Update Velevet-Fork.js
    Updated Hambueger Menu

--- Velevet-Fork.js
+++ Velevet-Fork.js
@@ -1,1 +1,13 @@
-console.log("Welcome to Velvet Fork!");
+ console.log("Welcome to Velvet Fork!");
+
+  function toggleMenu() {
+    const nav = document.getElementById("nav-menu");
+    nav.classList.toggle("active");
+  }
+
+  // Close menu after clicking a link
+  document.querySelectorAll("#nav-menu a").forEach(link => {
+    link.addEventListener("click", () => {
+      document.getElementById("nav-menu").classList.remove("active");
+    });
+  });
